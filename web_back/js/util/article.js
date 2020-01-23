3a1542d73da2c4dc0fc4d495c8cd19412de26811
ruijuanwang <1938608827@qq.com>
2020-01-23 15:30:23 +0800

    完成文章的类别和状态筛选并解决分页bug

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -3,8 +3,10 @@ var article = {
     // 获取文章列表数据
 
     // page:就是当前要获取的第几页数据
-    get: function(page) {
-        return $.get(APILIST.article_get, { 'page': page })
+    // type:就是当前文章的类型编号
+    // status:就是当前文章的状态
+    get: function(page, type, status) {
+        return $.get(APILIST.article_get, { 'page': page, 'type': type, 'state': status })
     },
     // // 获取文章添加分类
     // add: function(name, slug) {
--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -3,7 +3,7 @@
 
 
 // BASEUPL变量 存放所有接口的基地址
-var BASEUPL = 'http://192.168.2.105:8000';
+var BASEUPL = 'http://192.168.2.104:8000';
 
 // APILIST对象 用来保存所有的接口信息
 var APILIST = {
